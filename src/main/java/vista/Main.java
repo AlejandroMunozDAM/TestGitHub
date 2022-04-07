ed7bc7cf87a9ec9e49e5781c0a36cdcf60d0188d
AlejandroMunozDAM <1286957@alu.murciaeduca.es>
2022-04-07 16:41:26 +0200

    v2.0

--- src/main/java/vista/Main.java
+++ src/main/java/vista/Main.java
@@ -2,6 +2,7 @@ package vista;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println();
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
 	}
 }
